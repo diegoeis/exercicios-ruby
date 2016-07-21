987ae364842d8a14af62bc3da84c005e30008e89
Diego Eis <diego.eis@locaweb.com.br>
2016-07-21 10:20:03 -0300

    cleaning up to arabic exercise... i'm not good with logic problems... :-(

--- exercicio-roman-arabic.rb
+++ exercicio-roman-arabic.rb
@@ -1,45 +1,31 @@
 # Objetivo: Escreva uma classe que converta números romanos em números arábicos
 # RegExp para validar /^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
+# to execute in terminal: ruby -r "./exercicio-roman-arabic.rb" -e "converter = Converter.new; converter.to_arabic('XC')"
 class Converter
-  def initialize(roman_number)
+  def to_arabic(roman_number)
     @number = roman_number
-  end
-  def to_arabic
     numbers = {
-       1 => "I",
-       4 => "IV",
-       5 => "V",
-       9 => "IX",
-       10 => "X",
-       40 => "XL",
-       50 => "L",
-       90 => "XC",
-       100 => "C",
-       400 => "CD",
-       500 => "D",
-       900 => "CM",
-       1000 => "M"
+        "I" => 1,
+        "IV" => 4,
+        "V" => 5,
+        "IX" => 9,
+        "X" => 10,
+        "XL" => 40,
+        "L" => 50,
+        "XC" => 90,
+        "C" => 100,
+        "CD" => 400,
+        "D" => 500,
+        "CM" => 900,
+        "M" => 1000
     }
-  numbers.each do |roman, value|
-    if @number.start_with?(value)
-      puts roman
-      puts value
+
+    numbers.each do |roman, value|
+      if @number.start_with?(roman)
+        puts value
+    #     puts roman
+      end
     end
-  end
-    # i=1
-    # numbers.each do |roman, value|
-      # if numbers.include? @number
-      #   puts numbers[@number]
-      # end
-      # puts '---'
-      # puts key.to_s + ' => ' + value
-      # puts '---'
-    # end
-    # for x in (1..numbers.length)
-      
-    # end
-    # puts '---'
-    # puts @number
-    # puts '---'
+
   end
 end
